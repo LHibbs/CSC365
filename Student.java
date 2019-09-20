5655f7f9f9964e4c4b16daac6298a61d9e723e4d
Lauren Hibbs <lauren12412@gmail.com>
2019-09-20 11:43:33 -0700

    Student class

---
+++ Student.java
@@ -0,0 +1,44 @@
+public class Student {
+
+    private String stLastName;
+    private String stFirstName;
+    private int grade;
+    private int classroom;
+    private int bus;
+    private double GPA;
+    private String tLastName;
+    private String tFirstName; 
+
+    public String getStLastName(){
+        return stLastName;
+    }
+
+    public String getStFirstName(){
+        return stFirstName;
+    }
+
+    public int getGrade(){
+        return grade; 
+    }
+
+    public int getClassroom(){
+        return classroom;
+    }
+
+    public int getBus(){
+        return bus; 
+    }
+
+    public double getGPA(){
+        return GPA; 
+    }
+
+    public String getTLastName(){
+        return tLastName; 
+    }
+
+    public String getTFirstName(){
+        return tFirstName; 
+    }
+
+}
